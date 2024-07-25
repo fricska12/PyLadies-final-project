b31277af09467a7ad39a33e7ba93994f0d18aee3
Annamaria <fricska12@gmail.com>
2024-07-25 21:32:16 +0200

    fourth

--- snake.py
+++ snake.py
@@ -18,7 +18,25 @@ def draw_map(coordinates) :
 
     return dotti 
     
+def move(coordinates) :
     
+    direction = input("Wich direction to go? (n,s,e,w) : ")
+
+    if direction == "n" :
+        coordinates.append((coordinates[-1][0]-1, coordinates[-1][1]))
+    elif direction == "s" :
+        coordinates.append((coordinates[-1][0]+1, coordinates[-1][1]))
+    elif direction == "e" :
+        coordinates.append((coordinates[-1][0], coordinates[-1][1]+1))
+    elif direction == "w" :
+        coordinates.append((coordinates[-1][0], coordinates[-1][1]-1))
+    
+    return coordinates
+
 print("Snake example:")
 print(draw_map([(0,0), (0,1), (0,2), (0,3),(1,3), (2,3),(3,3), (3,4), (3,5)]))
+print("mooove")
+coordinates = [(2,2)]
+coords = move(coordinates)
+print(coords)
 
